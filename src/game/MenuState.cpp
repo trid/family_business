37e65dda6a9ba0a64855678964da513c65f55650
TriD <hovyakov@gmail.com>
2015-06-21 01:40:43 +0300

    MenuState

---
+++ src/game/MenuState.cpp
@@ -0,0 +1,5 @@
+//
+// Created by TriD on 21.06.2015.
+//
+
+#include "MenuState.h"
---
+++ src/game/MenuState.h
@@ -0,0 +1,15 @@
+//
+// Created by TriD on 21.06.2015.
+//
+
+#ifndef FAMILY_BUSINESS_MENUSTATE_H
+#define FAMILY_BUSINESS_MENUSTATE_H
+
+#include "../State.h"
+
+class MenuState: public State {
+
+};
+
+
+#endif //FAMILY_BUSINESS_MENUSTATE_H
