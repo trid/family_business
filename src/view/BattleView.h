e9bcb0ca325c3537cf7021997408fdfd298fd6db
TriD <hovyakov@gmail.com>
2015-06-21 01:44:40 +0300

    Battle View

---
+++ src/view/BattleView.h
@@ -0,0 +1,16 @@
+//
+// Created by TriD on 21.06.2015.
+//
+
+#ifndef FAMILY_BUSINESS_BATTLEVIEW_H
+#define FAMILY_BUSINESS_BATTLEVIEW_H
+
+
+#include "View.h"
+
+class BattleView: public View {
+
+};
+
+
+#endif //FAMILY_BUSINESS_BATTLEVIEW_H
