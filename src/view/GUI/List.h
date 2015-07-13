001e6421e2a8ef0ad14b8bc5ea8c4275e498ae50
TriD <hovyakov@gmail.com>
2015-07-14 01:46:59 +0300

    List widget stub

---
+++ src/view/GUI/List.h
@@ -0,0 +1,18 @@
+//
+// Created by TriD on 14.07.2015.
+//
+
+#ifndef FAMILY_BUSINESS_LIST_H
+#define FAMILY_BUSINESS_LIST_H
+
+
+#include "Widget.h"
+
+class List: public Widget {
+
+public:
+    List(int x, int y, int w, int h) : Widget(x, y, w, h) { }
+};
+
+
+#endif //FAMILY_BUSINESS_LIST_H
