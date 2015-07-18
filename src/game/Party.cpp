31ee746995cb03ffd4ad6768833399e3b9e67f85
TriD <hovyakov@gmail.com>
2015-07-18 18:57:48 +0300

    Party class

---
+++ src/game/Party.cpp
@@ -0,0 +1,5 @@
+//
+// Created by TriD on 18.07.2015.
+//
+
+#include "Party.h"
---
+++ src/game/Party.h
@@ -0,0 +1,17 @@
+//
+// Created by TriD on 18.07.2015.
+//
+
+#ifndef FAMILY_BUSINESS_PARTY_H
+#define FAMILY_BUSINESS_PARTY_H
+
+#include <vector>
+
+#include "../battle/BattleCreature.h"
+
+class Party {
+    std::vector<CreaturePtr> creatures;
+};
+
+
+#endif //FAMILY_BUSINESS_PARTY_H
