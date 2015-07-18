13bbe76114c7c4f124f3cde62de6bcd80ebecc76
TriD <hovyakov@gmail.com>
2015-07-19 00:29:04 +0300

    Show only alive characters

--- src/game/view/GUI/ChoseCharacterDialog.cpp
+++ src/game/view/GUI/ChoseCharacterDialog.cpp
@@ -9,14 +9,26 @@
 void ChoseCharacterDialog::setUp(FamilyPtr familyPtr) {
     clearWidgets();
 
-    addWidget(WidgetPtr(new CharacterButton(0, 0, getWidth(), 30, familyPtr->getFather(), callback)));
-    addWidget(WidgetPtr(new CharacterButton(0, 30, getWidth(), 30, familyPtr->getMother(), callback)));
+    int i = 0;
+
+    if (familyPtr->getFather()->isAlive()) {
+        addWidget(WidgetPtr(new CharacterButton(0, 0, getWidth(), 30, familyPtr->getFather(), callback)));
+        ++i;
+    }
+    if (familyPtr->getMother()->isAlive()) {
+        addWidget(WidgetPtr(new CharacterButton(0, i * 30, getWidth(), 30, familyPtr->getMother(), callback)));
+        ++i;
+    }
 
     Children& children = familyPtr->getChildren();
 
-    for (int i = 0; i < children.size(); i++) {
+    for (auto child: children) {
+        if (!child->isAlive()) {
+            continue;
+        }
         addWidget(WidgetPtr(
-                new CharacterButton(0, i * 30 + 60, getWidth(), 30, children[i], callback)));
+                new CharacterButton(0, i * 30, getWidth(), 30, child, callback)));
+        ++i;
     }
 
     unsigned int height = (children.size() + 2) * 30;
