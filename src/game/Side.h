a02c8ccc78a2e57dddcf4a14c15aad3ebb491552
TriD <hovyakov@gmail.com>
2015-07-18 19:21:49 +0300

    Side enumeration

---
+++ src/game/Side.h
@@ -0,0 +1,13 @@
+//
+// Created by TriD on 18.07.2015.
+//
+
+#ifndef FAMILY_BUSINESS_SIDE_H
+#define FAMILY_BUSINESS_SIDE_H
+
+enum class Side {
+    Player,
+    AI
+};
+
+#endif //FAMILY_BUSINESS_SIDE_H
