2dfbfc73ec09f9afe839001da7b945ca105a457a
TriD <hovyakov@gmail.com>
2015-08-31 02:41:42 +0300

    Label class can now change it's text

--- src/view/GUI/Label.cpp
+++ src/view/GUI/Label.cpp
@@ -11,10 +11,7 @@ Label::Label(int x, int y, int w, int h, const std::shared_ptr<Widget> &parent, 
                                                                                                                   parent),
                                                                                                            text(text) {
     font = FontsCache::getInstance().getFont("res/fonts/FreeMono.ttf", 14);
-    SDL_Surface *surface = TTF_RenderText_Solid(font, text.c_str(),
-                                                SDL_Color{0, 0, 0, 255});
-    label = SDL_CreateTextureFromSurface(Screen::getInstance().getRenderer(), surface);
-    SDL_FreeSurface(surface);
+    refreshText();
 }
 
 void Label::onRedraw(SDL_Renderer *renderer) {
@@ -22,4 +19,18 @@ void Label::onRedraw(SDL_Renderer *renderer) {
     SDL_Rect rect = SDL_Rect{0, 0, getWidth(), getHeight()};
     SDL_RenderFillRect(renderer, &rect);
     SDL_RenderCopy(renderer, label, nullptr, &rect);
+}
+
+void Label::setText(const std::string &text) {
+    this->text = text;
+    refreshText();
+}
+
+void Label::refreshText() {
+    if (label) {
+        SDL_DestroyTexture(label);
+    }
+    SDL_Surface *surface = TTF_RenderText_Solid(font, text.c_str(), SDL_Color{0, 0, 0, 255});
+    label = SDL_CreateTextureFromSurface(Screen::getInstance().getRenderer(), surface);
+    SDL_FreeSurface(surface);
 }
--- src/view/GUI/Label.h
+++ src/view/GUI/Label.h
@@ -12,14 +12,16 @@
 class Label : public Widget {
 private:
     std::string text;
-    SDL_Texture* label;
+    SDL_Texture* label{nullptr};
     TTF_Font* font;
+    void refreshText();
 public:
     Label(int x, int y, int w, int h, const std::shared_ptr<Widget> &parent, const std::string &text);
-
-private:
-    virtual void onRedraw(SDL_Renderer *renderer);
+    void setText(const std::string &text);
+protected:
+    virtual void onRedraw(SDL_Renderer *renderer) override;
 };
 
+using LabelPtr = std::shared_ptr<Label>;
 
 #endif //FAMILY_BUSINESS_LABEL_H
